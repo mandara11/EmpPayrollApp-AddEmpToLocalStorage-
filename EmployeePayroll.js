6ecc676db2647cc320fd21db7c5d815a00795694
mandara11 <mandaram2345@gmail.com>
2022-09-20 22:06:44 +0530

    Added:UC2SetEventListners

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -45,13 +45,19 @@ class EmployeePayroll {
 
     get startDate() { return this._startDate; }
     set startDate(startDate) {
+        let now = new Date();
+        if (startDate > now) throw 'Start Date is a Future Date!';
+        var diff = Math.abs(now.getTime() - startDate.getTime());
+        if (diff / (1000 * 60 * 60 * 24) > 30)
+            throw 'Start Date is beyond 30 Days!';
+        this._startDate = startDate;
         this._startDate = startDate;
     }
     // toString() method
     toString() {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
         const empDate = !this.startDate ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
-        return "id=" + this.id + ", name=" + this.name + ", gender=" + this.gender + 
+        return "id=" + this.id + ", name=" + this.name + ", gender=" + this.gender +
             ", profilePic=" + this.profilePic + ", department=" + this.department +
             ", salary=" + this.salary + ", startDate=" + empDate + ", note=" + this.note;
     }
